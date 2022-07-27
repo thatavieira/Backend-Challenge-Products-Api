c4eed6e31c080ea55b4e1fd11caeba4fbcb03720
thatavieira <thagurgel0909@gmail.com>
2022-07-26 21:49:57 -0300

    improvements db config

--- src/db.py
+++ src/db.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from decouple import config
 
-sqlalchemy_database_url = "postgresql+psycopg2://gpjbvsfn:AJrA-NLEG4tMYWKoA3FACY9-nmx7y9LP@isilo.db.elephantsql.com/gpjbvsfn"
+user = config('POSTGRES_USER')
+password = config('POSTGRES_PASSWORD')
+host = config('POSTGRES_HOST')
+port = config('POSTGRES_PORT')
+db = config('POSTGRES_DB')
 
-engine = create_engine(sqlalchemy_database_url)
+database_url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}" 
+
+engine = create_engine(database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
